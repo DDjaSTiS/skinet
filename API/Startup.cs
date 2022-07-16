2b138b7e115050ead99928dd5609e9c6d829150d
Evgeniy Potopnin <evgeniy.potopnin@spbpu.com>
2022-07-16 11:04:49 +0300

    End of section 6

--- API/Startup.cs
+++ API/Startup.cs
@@ -18,7 +18,7 @@ namespace API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            
+
             services.AddAutoMapper(typeof(MappingProfiles));
             services.AddControllers();
             services.AddDbContext<StoreContext>(x =>
@@ -27,15 +27,22 @@ namespace API
             services.AddApplicationServices();
 
             services.AddSwaggerDocumentation();
+
+            services.AddCors(opt =>
+            {
+                opt.AddPolicy("CorsPolicy", policy =>
+                {
+                    policy.AllowAnyHeader()
+                    .AllowAnyMethod()
+                    .WithOrigins("https://localhost:4200");
+                });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             app.UseMiddleware<ExceptionMiddleware>();
-
-           
-
 
             app.UseStatusCodePagesWithReExecute("/errors/{0}");
 
@@ -44,6 +51,8 @@ namespace API
             app.UseRouting();
 
             app.UseStaticFiles();
+
+            app.UseCors("CorsPolicy"); 
 
             app.UseAuthorization();
 
